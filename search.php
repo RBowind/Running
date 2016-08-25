d0de2c5091bd54cc4e22222ec3eee1634fbea8cf
RBowind <270342879@qq.com>
2016-08-25 16:30:12 +0800

    查询用户是否存在

---
+++ search.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: RBowi
+ * * 参数： get:act
+ * 功能：实现查询用户是否存在
+ * 通过get方法传递act 的值 account 来实现相应的功能，post方法传递值，JSON格式，成功会返回200或传递的值，否则会报错。
+
+ * 根目录： 121.42.162.214/RunningTogether/
+ * Date: 2016/7/12
+ * Time: 10:00
+ */
+
+require_once 'header.php';
+
+    $account = $_GET['account'];
+    $pdo = new PdoMySQL();
+
+
+    try {
+        $pdo -> find($tables,"account = '.$account.'");
+        $result = true ;
+        return $result;
+    }catch (PDOException $e){
+        echo $sql . "<br>" . $e->getMessage();
+    }
