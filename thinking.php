abb44e9fdfead2915eb8cad086d1b4a580f51f45
RBowind <270342879@qq.com>
2016-08-25 13:30:29 +0800

    正在完善发布感想功能

---
+++ thinking.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 赵天歌
+ * Date: 2016/8/19
+ * Time: 0:33
+ *功能：实现发布感想的功能。此功能也必须先登录来保持session。
+ */
+    require_once 'header.php';
+    if(!isset($_SESSION['account']))
+    {
+    	exit("echo '请注册或登录';");
+    }
+
+
+    
+
+
+   
+
