37382d91732a23c960dff4316e94d4b6e87d01ab
RBowind <270342879@qq.com>
2016-09-11 20:53:28 +0800

    解决了一维数组 Json 输出中文乱码的问题

--- header.php
+++ header.php
@@ -16,11 +16,19 @@ header('Access-Control-Allow-Headers: "Content-Type, Access-Control-Allow-Header
 $postJson = file_get_contents('php://input');
 $_POST = json_decode($postJson, true);
 
-// 封装了 解决json输出中文乱码的问题的方法。
-function Json($array){
+// 封装了 解决json输出中文乱码的问题的方法。仅适用于一维数组。
+function JsonEcho($array){
     foreach ( $array as $key => $value ) {
         $array[$key] = urlencode ( $value );
     }
     echo  urldecode (json_encode ($array ));
 }
+
+/*function Json($array){
+    foreach ( $array as $key => $value ) {
+        $array[$key] = urlencode ( $value );
+    }
+       urldecode (json_encode ($array ));
+}*/
+
 
