f1fc35acdb0fac40749aba259a5a07afe505d14b
RBowind <270342879@qq.com>
2016-09-11 21:51:26 +0800

    查询用户是否存在，返回结果已经json化

--- search.php
+++ search.php
@@ -1,12 +1,13 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: RBowi
- * * 参数： get:act
+ * User: RBowind
+ * * 参数： get:account=$account 把用户名用get方法 account的值来传递
  * 功能：实现查询用户是否存在
- * 通过get方法传递act 的值 account 来实现相应的功能，post方法传递值，JSON格式，成功会返回200或传递的值，否则会报错。
-
- * 根目录： 121.42.162.214/RunningTogether/
+ * 通过get方法传递account 的值 account 来查询用户是否存在。
+ * 若存在：返回 "wrong": "The account has been registered";
+ * 不存在，返回 布尔值 true；也就是 1,也就是这种情况可以注册。
+ *
  * Date: 2016/7/12
  * Time: 10:00
  */
@@ -15,14 +16,16 @@ require_once 'header.php';
 
     $account = $_GET['account'];
     $pdo = new PdoMySQL();
-    $tables = 'user';
-
     try {
-        if($pdo -> find($tables,"account = '.$account.'")){
-            echo true;
+        if($pdo -> find('user','account = "'.$account.'"'))
+        {
+            $result = [
+                'wrong' => "The account has been registered"
+            ];
+            echo json_encode($result);
         }else{
-            echo false;
-            echo "用户不存在";
+            echo true ;
+
         }
 
     }catch (PDOException $e){
