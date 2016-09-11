5812fbb3502a1d7efaab1e20e773e9aaa514de74
RBowind <270342879@qq.com>
2016-09-11 18:01:32 +0800

    解决了Json 输出中文乱码的问题

--- header.php
+++ header.php
@@ -5,6 +5,10 @@
  * Date: 2016/7/16
  * Time: 20:14
  */
+
+require_once 'PdoMySQL.class.php';
+require_once 'config.php';
+
 session_start();
 header('content-type:text/json;charset=utf-8');
 header('Access-Control-Allow-Headers: "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"');
@@ -12,6 +16,11 @@ header('Access-Control-Allow-Headers: "Content-Type, Access-Control-Allow-Header
 $postJson = file_get_contents('php://input');
 $_POST = json_decode($postJson, true);
 
+// 封装了 解决json输出中文乱码的问题的方法。
+function Json($array){
+    foreach ( $array as $key => $value ) {
+        $array[$key] = urlencode ( $value );
+    }
+    echo  urldecode (json_encode ($array ));
+}
 
-require_once 'PdoMySQL.class.php';
-require_once 'config.php';
