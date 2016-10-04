7234b550b47f9811d064a16a862e02193f2fdcb5
RBowind <270342879@qq.com>
2016-10-04 14:44:46 +0800

    config.php

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by 赵天歌 on 2016/10/4.
+ * Time: 14:43
+ */
+define("DB_HOST",'127.0.0.1');
+define("DB_USER",'root');
+define('DB_PWD','');
+define('DB_NAME','running');
+define('DB_PORT','3306');
+define('DB_TYPE','mysql');
+define('DB_CHARSET','utf8');
