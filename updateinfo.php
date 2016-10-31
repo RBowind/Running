8bbfca5306b87379e47b083a3bed2ff40e8dcc21
RBowind <270342879@qq.com>
2016-10-31 14:15:32 +0800

    修改相关文档说明

--- updateinfo.php
+++ updateinfo.php
@@ -7,7 +7,7 @@
  * 功能：实现用户信息的更新
  * 通过get方法传递act 的值 update 来实现相应的功能，post方法传递值，JSON格式，成功会返回200或传递的值，否则会报错。
  * POSTman 里面要用json格式发送请求，也就是 raw里面用JSON格式 ，而不是 x-www-form-urlencoded 格式。不然会有问题。
- * 根目录： 121.42.162.214/RunningTogether/
+ * 根目录：??
  * Date: 2016/7/16
  * Time: 21:26
  */
--- userinfo.php
+++ userinfo.php
@@ -7,7 +7,7 @@
  * 功能：实现用户信息的添加
  * 通过get方法传递act 的值 addinfo 来实现相应的功能，post方法传递值，JSON格式，成功会返回200或传递的值，否则会报错。
  * POSTman 里面要用json格式发送请求，也就是 raw里面用JSON格式 ，而不是 x-www-form-urlencoded 格式。不然会有问题。
- * 根目录： 121.42.162.214/RunningTogether/
+ * 根目录：??
  * Date: 2016/7/16
  * Time: 20:13
  */
@@ -15,7 +15,7 @@ require_once 'header.php';
 
 $pdo = new PdoMySQL();
 
-$act = $_GET['act'];
+@$act = $_GET['act'];
 
 /*$account = $_SESSION['account'];
 $name = $_POST['name'];
