5593ccd1fc07b3fc460bf05cac2bd9e2661b0657
RBowind <270342879@qq.com>
2016-11-04 11:39:11 +0800

    修改备注

--- test.php
+++ test.php
@@ -5,6 +5,9 @@
  *把当前用户的用户名用 get方法传过来。参数名为：account。
  *传递过来后，这里会返回类似这样的（这里我传的值是 account=赵天歌）：{"array":[{"becanceled":"1"},{"becanceled":"0"},{"becanceled":"0"}]}
  * 定时发送请求,如果活动取消了，becanceled会变成1，那样返回的值就回和上次不同，意味活动已经发生改变，这时候提示用户去 “我的参与” 看哪些活动被取消了。
+ * account:赵天歌
+ * password:123456
+ * 用上面这个账号测试。
  */
 
 require_once 'header.php';
